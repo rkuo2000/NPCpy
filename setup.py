2e2e241cce75a36207dd04562edd1c05daf2cd2e
Chris Agostino <cjp.agostino@gmail.com>
2025-09-17 13:11:45 -0400

    pyver

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ extra_files = package_files("npcpy/npc_team/")
 
 setup(
     name="npcpy",
-    version="1.2.15",
+    version="1.2.16",
     packages=find_packages(exclude=["tests*"]),
     install_requires=base_requirements,  
     extras_require={
