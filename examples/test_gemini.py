f3e140eeadb3c9fa2c1c2bde994e9e6ca0a53629
Richard Kuo <rkuo2000@gmail.com>
2025-09-23 02:44:15 +0800

    Add files via upload

--- examples/test_gemini.py
+++ examples/test_gemini.py
@@ -5,6 +5,7 @@ simon = NPC(
           model='gemini-2.5-flash',
           provider='gemini'
           )
-response = simon.get_llm_response("What is the most important territory to retain in the Andes mountains?")
+#response = simon.get_llm_response("What is the most important territory to retain in the Andes mountains?")
+response = simon.get_llm_response("What is your name? and Where do you live?")
 print(response['response'])
 
